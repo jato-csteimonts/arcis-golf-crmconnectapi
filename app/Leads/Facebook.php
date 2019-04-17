94c479c60e2f87520691927604837a1cca303d31
Christopher Steimonts <chris.steimonts@gmail.com>
2019-04-17 07:26:03 -0700

    Facbook lead tweaks and fixes

--- app/Leads/Facebook.php
+++ app/Leads/Facebook.php
@@ -22,19 +22,19 @@ class Facebook extends Base {
 			'private'
 		];
 
-		$tmp_data = explode("_", $data['campaign_attribution'], 2);
+		$tmp_data = explode("_", $data['campaign_name'], 2);
 
 		$name_info                   = explode(" ", $data['full_name'], 2);
 		$out                         = [];
 		$out['campaign_attribution'] = $data['campaign_name'];
-		$out['sub_type']             = in_array(strtolower($tmp_data[2]), $sub_types) ? strtolower($tmp_data[2]) : "event";
+		$out['sub_type']             = in_array(strtolower(isset($tmp_data[2]) ? $tmp_data[2] : ""), $sub_types) ? strtolower($tmp_data[2]) : "event";
 		$out['source']               = "Facebook Lead Ad: " . $data['campaign_name'];
-		$out['email']                = $data['email'];
+		$out['email']                = $data['email'] ?? "";
 		$out['first_name']           = $name_info[0];
 		$out['last_name']            = $name_info[1] ?? "No Last Name Provided";
-		$out['phone']                = preg_replace("/([^0-9]+)/", "", $data['phone_number']);
+		$out['phone']                = preg_replace("/([^0-9]+)/", "", $data['phone_number'] ?? "");
 		$out['last_name']            = $name_info[1] ?? "No Last Name Provided";
-		$out['company_title']        = $data['company_name'];
+		$out['company_title']        = isset($data['company_name']) ? $data['company_name'] : "";
 
 		switch(true) {
 			case !$out['email']:
