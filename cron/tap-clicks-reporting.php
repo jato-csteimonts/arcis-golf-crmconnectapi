4dea458423f89d6e9131052cb25b054e42a50d1a
Christopher Steimonts <chris.steimonts@gmail.com>
2020-01-15 17:14:39 -0800

    tap clicks report file

--- cron/tap-clicks-reporting.php
+++ cron/tap-clicks-reporting.php
@@ -8,9 +8,55 @@ $app->make(Kernel::class)->bootstrap();
 
 $ServiceProvider = new \App\ServiceProviders\ReserveInteractive();
 
-foreach(App\Leads\Base::whereNull("duplicate_of")->where("created_at", ">=", "2020-01-01T00:00:00")->orderBy("created_at", "DESC")->get() as $index => $Lead) {
+$headers = [
+	"lead_id",
+	"club_id",
+	"club_name",
+	"lead_first_name",
+	"lead_last_name",
+	"lead_email",
+	"medium",
+	"revenue_category",
+	"campaign_term",
+	"campaign_name",
+	"status",
+	"next_action",
+	"converted",
+	"assigned_to_name",
+	"assigned_to_email",
+	"reserve_error",
+	"created_at"
+];
 
-    print(($index+1) . "\n");
+$Leads = [];
+
+foreach(App\Leads\Base::whereNull("duplicate_of")
+                      ->where("created_at", ">=", "2020-01-01T00:00:00")
+                      ->where("sub_type", "!=", "member")
+                      ->orderBy("created_at", "DESC")
+                      ->get() as $index => $Lead) {
+
+	/**
+	if($index >= 2) {
+		break;
+	}
+	**/
+
+	//print_r($Lead->toArray());
+
+	$CurrentLead = [];
+	$CurrentLead['lead_id'] = $Lead->id;
+	$CurrentLead['club_id'] = \App\Club::find($Lead->club_id)->site_code;
+	$CurrentLead['club_name'] = \App\Club::find($Lead->club_id)->name;
+	$CurrentLead['lead_first_name'] = $Lead->first_name;
+	$CurrentLead['lead_last_name'] = $Lead->last_name;
+	$CurrentLead['lead_email'] = $Lead->email;
+	$CurrentLead['medium'] = $Lead->campaign_medium_id ? \App\CampaignMedium::find($Lead->campaign_medium_id)->code : "";
+	$CurrentLead['campaign_term'] = $Lead->campaign_term_id ? \App\CampaignTerm::find($Lead->campaign_term_id)->code : "";
+	$CurrentLead['campaign_name'] = $Lead->campaign_name_id ? \App\CampaignName::find($Lead->campaign_name_id)->slug : "";
+	$CurrentLead['revenue_category'] = str_pad($Lead->revenue_category, 2, "0", STR_PAD_LEFT);
+
+    print(($index+1) . "...");
 
     $data = unserialize($Lead->data);
 
@@ -44,7 +90,6 @@ foreach(App\Leads\Base::whereNull("duplicate_of")->where("created_at", ">=", "20
                 'requestName'  => $request,
                 'requestGuid'  => md5(date('YmdHis')),
                 'maxResults'   => 1,
-                //'firstResult'  => $offset,
                 'orderByField' => "localCreationDate",
                 'asc'          => false,
                 'filters'      => count($filters) ? "[[" . implode("],[", $filters) . "]]" : "",
@@ -55,282 +100,70 @@ foreach(App\Leads\Base::whereNull("duplicate_of")->where("created_at", ">=", "20
 
         $response = $ServiceProvider->request("GET",NULL, $args );
 
-        //print_r($response);
+        if(count($response['Body']->results)) {
+	        $CurrentLead['status']        = $response['Body']->results[0][array_search("leadStatus", $response['Body']->header)];
+	        $CurrentLead['next_action']   = $response['Body']->results[0][array_search("lastActivityInfo", $response['Body']->header)];
+	        $CurrentLead['converted']     = $response['Body']->results[0][array_search("converted", $response['Body']->header)];
+	        $CurrentLead['created_at']    = $response['Body']->results[0][array_search("creationDate", $response['Body']->header)];
+        } else {
+	        $CurrentLead['created_at'] = strftime("%Y-%m-%dT%H:%M:%S", strtotime($Lead->created_at));
+        }
 
-    }
-
-}
-
-
-exit;
-
-
-
-
-
-
-
-$Options = getopt("d::s::", []);
-
-$filters = [];
-
-if ( isset($Options['d']) && $Options['d'] != "D" ) {
-    @list( $start, $stop ) = explode( ",", $Options['d'] );
-    $filters[] = "'creationDate', 'GREATER_THAN_OR_EQUAL_TO', '" . strftime("%m/%d/%Y %I:%M %p", strtotime($start)) . "'";
-    if($stop) {
-        $filters[] = "'creationDate', 'LESS_THAN', '" . strftime("%m/%d/%Y %I:%M %p", strtotime($stop)) . "'";
     } else {
-        $filters[] = "'creationDate', 'LESS_THAN', '" . strftime("%m/%d/%Y 12:00 AM", strtotime("Today")) . "'";
-    }
-} else {
-    $filters[] = "'creationDate', 'GREATER_THAN_OR_EQUAL_TO', '" . strftime("%m/%d/%Y 12:00 AM", strtotime("Yesterday")) . "'";
-    $filters[] = "'creationDate', 'LESS_THAN', '" . strftime("%m/%d/%Y 12:00 AM", strtotime("Today")) . "'";
-}
 
-$ServiceProvider = new \App\ServiceProviders\ReserveInteractive();
+	    $CurrentLead['created_at'] = strftime("%Y-%m-%dT%H:%M:%S", strtotime($Lead->created_at));
 
-$data = [];
+    	try {
+		    $ReserveInteractive = \App\ReserveInteractive::where("lead_id", $Lead->id)->firstOrFail();
+		    $ri_response        = json_decode($ReserveInteractive->response);
 
-/**
-$data[] = [
-"Unique ID",
-"Site",
-"Site Code",
-"Attribution Code",
-"Lead Status",
-"Lead First Name",
-"Lead Last Name",
-"Lead Email",
-"Created Date",
-"Lead Type",
-"utm_content",
-"utm_medium",
-"utm_name",
-"utm_term",
-"utm_source",
-];
- **/
+		    if($ri_response[0]->status == "Failed") {
+			    $CurrentLead['reserve_error'] = trim(implode(" ", $ri_response[0]->messages));
+		    }
+	    } catch (\Exception $e) {}
 
-$data[] = [
-    "Site_Code",
-    "Ri_Lead_Id",
-    "created_date",
-    "attribution_code",
-    "site",
-    "lead_type",
-    "lead_sub_type",
-    "lead_source",
-    "lead_status",
-    "utm_content",
-    "utm_medium",
-    "utm_name",
-    "utm_term",
-    "utm_source",
-];
-
-foreach(["tap_clicks_member_leads"/**,"tap_clicks_event_leads"**/] as $request) {
-
-    $lead_type = preg_match("/_member_/", $request) ? "member" : "event";
-    $requests  = 0;
-    $interval  = 100;
-    $count     = 0;
-    $offset    = $interval*$requests;
-
-    $args = [
-        'auth' => [
-            env('RESERVE_INTERACTIVE_USERNAME'),
-            env('RESERVE_INTERACTIVE_PASSWORD')
-        ],
-        'query' => [
-            'requestName'  => $request,
-            'requestGuid'  => md5(date('YmdHis')),
-            'maxResults'   => $interval,
-            'firstResult'  => $offset,
-            'orderByField' => "localCreationDate",
-            'asc'          => false,
-            'filters'      => count($filters) ? "[[" . implode("],[", $filters) . "]]" : "",
-        ],
-    ];
-
-    try {
-
-        $response = $ServiceProvider->request("GET",NULL, $args );
-
-        do {
-
-            foreach($response['Body']->results as $index => $record) {
-
-                print($offset+$index+1 . ": {$record[7]} {$record[8]} ({$record[9]}) - {$record[4]}\n");
-
-                $lead      = new stdClass();
-                $lead_data = [];
-
-                try {
-                    $ri        = \App\ReserveInteractive::where("response", "LIKE", "%\"{$record[0]}\"%")->firstOrFail();
-                    $lead      = \App\Lead::find($ri->lead_id);
-                    $lead_data = unserialize($lead->data);
-                    //print_r($lead->toArray());
-                } catch (Exception $e) {
-
-
-
-
-
-
-                    print_r($record);
-                    //exit;
-
-                }
-
-                continue;
-
-                //print_r($lead_data);
-
-                $attribution_code = $lead_data['utm_term'] ?? "";
-
-                if(!preg_match("/^([\d]{7})\-([\d]{4})$/", trim($attribution_code))) {
-
-                    $old_attribution_code = $attribution_code;
-                    $site_code            = $record[2];
-
-                    switch(true) {
-                        case ($lead->type ?? null) == "beloandco":
-                            $medium_verbose = "website";
-                            break;
-                        case ($lead->type ?? null) == "unbounce":
-                            switch(true) {
-                                case ($lead_data['utm_medium'] ?? null) == "facebook":
-                                case ($lead_data['utm_medium'] ?? null) == "instagram":
-                                case ($lead_data['utm_medium'] ?? null) == "eltoro":
-                                case ($lead_data['utm_medium'] ?? null) == "gmb":
-                                case ($lead_data['utm_medium'] ?? null) == "email":
-                                    $medium_verbose = $lead_data['utm_medium'];
-                                    break;
-                                case ($lead_data['utm_medium'] ?? null) == "website":
-                                case ($lead_data['utm_medium'] ?? null) == "cta_box":
-                                default:
-                                    $medium_verbose = "website";
-                                    break;
-                            }
-                            break;
-                        case strstr(strtolower($record[18]), "wedding"):
-                            switch(true) {
-                                case strstr(strtolower($record[19]), "knot"):
-                                    $medium_verbose = "theknot";
-                                    break;
-                                case strstr(strtolower($record[19]), "wire"):
-                                    $medium_verbose = "weddingwire";
-                                    break;
-                            }
-                            break;
-                    }
-
-                    $medium = null;
-                    if($medium_verbose ?? false) {
-                        $medium = \App\CampaignMedium::where("slug", $medium_verbose)->value("code");
-                        print("Medium: {$medium_verbose} ({$medium})\n");
-                    } else {
-                        print("Missing Medium, skipping {$record[7]} {$record[8]} ({$record[9]}) - {$record[4]}.....\n");
-                        continue;
-                    }
-
-                    switch(true) {
-                        case strstr(strtolower($record[18]), "wedding"):
-                            $revenue_category = "02";
-                            break;
-                        case $lead->sub_type ?? null == "member":
-                            $revenue_category = "01";
-                            break;
-                        case $lead->sub_type ?? null == "wedding":
-                        case $lead->sub_type ?? null == "private":
-                            $revenue_category = "03";
-                            break;
-                    }
-
-                    $attribution_code = "{$site_code}{$medium}{$revenue_category}";
-
-                    //print("Old: {$old_attribution_code}\n");
-                    //print("New: {$attribution_code}\n");
-                    //print("---------------------\n");
-
-                }
-
-                continue;
-
-                //print_r($lead_data);
-
-                $data[] = [
-                    $record[2],
-                    $record[0],
-                    strftime("%m/%d/%Y", strtotime($record[4])),
-                    //$record[6],
-                    $lead_data['utm_term'] ?? "",
-                    "{$record[2]} ({$record[3]})",
-                    $lead_type,
-                    $lead->sub_type ?? "",
-                    $lead->type ?? "",
-                    $record[5],
-                    $lead_data['utm_content'] ?? "",
-                    $lead_data['utm_medium'] ?? "",
-                    $lead_data['utm_name'] ?? "",
-                    $lead_data['utm_term'] ?? "",
-                    $lead_data['utm_source'] ?? "",
-                ];
-
-                /**
-                $data[] = [
-                $record[0],
-                $record[2],
-                "{$record[1]} ({$record[3]})",
-                $record[6],
-                $record[5],
-                $record[7],
-                $record[8],
-                $record[9],
-                strftime("%m/%d/%Y", strtotime($record[4])),
-                $record[10],
-                $lead_data['utm_content'] ?? "",
-                $lead_data['utm_medium'] ?? "",
-                $lead_data['utm_namme'] ?? "",
-                $lead_data['utm_term'] ?? "",
-                $lead_data['utm_source'] ?? "",
-                ];
-                 **/
-
-            }
-
-            $requests++;
-            $offset = $interval*$requests;
-            $args['query']['firstResult'] = $offset;
-
-            $response = $ServiceProvider->request("GET",NULL, $args );
-
-        } while(count($response['Body']->results));
-
-        if(isset($Options['s']) && $Options['s']) {
-            print_r($data);
-        }
-
-        $fp = fopen('./tapclicks.csv', 'w');
-
-        foreach ($data as $row) {
-            fputcsv($fp, $row);
-        }
-
-        fclose($fp);
-
-    } catch (\GuzzleHttp\Exception\ServerException $e) {
-        print_r($e->getResponse()->getBody()->getContents());
-    } catch (\GuzzleHttp\Exception\ClientException $e) {
-        print_r($e->getResponse()->getBody()->getContents());
     }
 
-    break;
+    switch(true) {
+	    case preg_match("/ CST$/", $CurrentLead['created_at']):
+		    date_default_timezone_set("America/Chicago");
+		    break;
+	    case preg_match("/ MST$/", $CurrentLead['created_at']):
+		    date_default_timezone_set("America/Denver");
+		    break;
+	    case preg_match("/ PST$/", $CurrentLead['created_at']):
+		    date_default_timezone_set("America/Los_Angeles");
+		    break;
+	    case preg_match("/ EST$/", $CurrentLead['created_at']):
+	    default:
+		    date_default_timezone_set("America/New_York");
+	    	break;
+    }
 
+	$CurrentLead['created_at']        = strftime("%Y-%m-%dT%H:%M:%S", strtotime($CurrentLead['created_at']));
+	$CurrentLead['assigned_to_name']  = \App\User::find($Lead->owner)->name;
+	$CurrentLead['assigned_to_email'] = \App\User::find($Lead->owner)->email;
+
+	$tmp = [];
+	foreach($headers as $index => $field) {
+		$tmp[$index] = isset($CurrentLead[$field]) ? $CurrentLead[$field] : "";
+	}
+	$Leads[] = $tmp;
 }
+
+$Leads = array_merge([$headers], $Leads);
+
+$fp = fopen('./tapclicks-reports.csv', 'w');
+
+foreach ($Leads as $row) {
+	//print_r($row);
+	fputcsv($fp, $row);
+}
+
+fclose($fp);
 
 if(!isset($Options['s']) || !$Options['s']) {
-    Storage::disk('ftp')->put('tapclicks.csv', fopen('./tapclicks.csv', 'r+'));
+    Storage::disk('ftp')->put('tapclicks-reports.csv', fopen('./tapclicks-reports.csv', 'r+'));
 }
 
 ?>
